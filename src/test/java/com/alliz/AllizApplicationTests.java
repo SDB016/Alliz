b85a9da28e803abecb4862441718a6e982105dc8
SDB016 <dongbin4013@naver.com>
2021-08-10 16:22:35 +0900

    Project Start

---
+++ src/test/java/com/alliz/AllizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alliz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
