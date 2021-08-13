f1d0be89d76ba41262474def2e8d0c620aeadeb0
SDB016 <dongbin4013@naver.com>
2021-08-14 02:20:06 +0900

    style: Update Sign up view

--- src/main/java/com/alliz/account/SignUpForm.java
+++ src/main/java/com/alliz/account/SignUpForm.java
@@ -11,8 +11,7 @@ import javax.validation.constraints.Pattern;
 public class SignUpForm {
 
     @NotBlank
-    @Length(min=3, max = 20, message = "3 ~ 20자를 입력하세요.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$", message = "한글 또는 영어, 숫자를 이용한 닉네임을 입력하세요.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$", message = "3 ~ 20자, 한글 또는 영어, 숫자를 이용한 닉네임을 입력하세요.")
     private String nickname;
 
     @Email
@@ -20,12 +19,10 @@ public class SignUpForm {
     private String email;
 
     @NotBlank
-    @Length(min = 8, max = 50, message = "8자 이상 입력하세요.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "영어와 숫자를 최소 한 개 이상 사용하세요.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[A-Za-z\\d]{8,50}$", message = "8자 이상, 영어와 숫자를 최소 한 개 이상 사용하세요.")
     private String password;
 
     @NotBlank
-    @Length(min = 8, max = 50)
     private String passwordConfirm;
 }
 
