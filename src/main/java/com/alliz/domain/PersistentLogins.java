7e70db722bdf3f1ce95eb2ec5717edc5604722a0
SDB016 <dongbin4013@naver.com>
2021-08-20 17:40:53 +0900

    feat: Create Remember-me

---
+++ src/main/java/com/alliz/domain/PersistentLogins.java
@@ -0,0 +1,26 @@
+package com.alliz.domain;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+public class PersistentLogins {
+    @Id
+    @Column(length = 64)
+    private String series;
+
+    @Column(nullable = false, length = 64)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String token;
+
+    @Column(name = "last_used", nullable = false)
+    private LocalDateTime lastUsed;
+}
