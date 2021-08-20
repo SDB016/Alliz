5bcb9aa275b6b52360d319cdc65ca592f367b575
SDB016 <dongbin4013@naver.com>
2021-08-20 16:27:25 +0900

    feat: Create @WithAccount

---
+++ src/main/java/com/alliz/WithAccountSecurityContextFactory.java
@@ -0,0 +1,37 @@
+package com.alliz;
+
+import com.alliz.account.AccountService;
+import com.alliz.account.SignUpForm;
+import com.alliz.account.UserAccount;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+@RequiredArgsConstructor
+public class WithAccountSecurityContextFactory implements WithSecurityContextFactory<WithAccount> {
+
+    private final AccountService accountService;
+
+    @Override
+    public SecurityContext createSecurityContext(WithAccount withAccount) {
+        String nickname = withAccount.value();
+
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname(nickname);
+        signUpForm.setEmail(nickname + "@email.com");
+        signUpForm.setPassword("aa123123");
+        signUpForm.setPasswordConfirm("aa123123");
+        accountService.processNewAccount(signUpForm);
+
+        UserDetails principle = accountService.loadUserByUsername(nickname);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principle, principle.getPassword(), principle.getAuthorities());
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+
+        return context;
+    }
+}
