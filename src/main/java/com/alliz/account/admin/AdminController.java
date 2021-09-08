07f01057ee858b50b49640bb4a6991f15b0de18e
SDB016 <dongbin4013@naver.com>
2021-09-08 19:34:05 +0900

    feat: Edit & Remove Reservation

--- src/main/java/com/alliz/account/admin/AdminController.java
+++ src/main/java/com/alliz/account/admin/AdminController.java
@@ -59,6 +59,41 @@ public class AdminController {
         return "redirect:/admin/new-reservations";
     }
 
+    @GetMapping("/admin/reservation/remove/{id}")
+    public String removeReservation(@CurrentAccount Account account, @PathVariable Long id) {
+        adminService.checkRole(account);
+        adminService.removeReservationById(id);
+        return "redirect:/admin/reservations";
+    }
+
+    @GetMapping("/admin/reservation/edit/{id}")
+    public String editReservation(@CurrentAccount Account account, @PathVariable Long id, Model model) {
+        adminService.checkRole(account);
+        Reservation reservation = adminService.findReservationById(id);
+        model.addAttribute(modelMapper.map(reservation, ReservationForm.class));
+        model.addAttribute(account);
+        model.addAttribute("reservationId", id);
+        return "admin/edit-reservations";
+    }
+
+    @PostMapping("/admin/reservation/edit/{id}")
+    public String editReservation(@CurrentAccount Account account,@PathVariable Long id,
+                                  @Valid ReservationForm form, Errors errors, Model model, RedirectAttributes attributes) {
+        adminService.checkRole(account);
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            return "admin/edit-reservations";
+        }
+        if (adminService.isAlreadyReservation(form.getReservationTime())) {
+            model.addAttribute("error", "이 시간의 배차는 이미 등록되었습니다. 배차조회를 확인하세요.");
+            model.addAttribute(account);
+            return "admin/edit-reservations";
+        }
+        adminService.editReservation(id, form);
+        model.addAttribute(account);
+        return "redirect:/admin/reservations";
+    }
+
     @GetMapping("/admin/reservations")
     public String reservationsView(@CurrentAccount Account account, Model model) {
         adminService.checkRole(account);
--- src/main/java/com/alliz/account/admin/AdminService.java
+++ src/main/java/com/alliz/account/admin/AdminService.java
@@ -5,10 +5,12 @@ import com.alliz.domain.Role;
 import com.alliz.reservation.Reservation;
 import com.alliz.reservation.ReservationRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.file.AccessDeniedException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +18,15 @@ import java.nio.file.AccessDeniedException;
 public class AdminService {
 
     private final ReservationRepository reservationRepository;
+    private final ModelMapper modelMapper;
 
-    public void checkRole(Account account) throws AccessDeniedException {
+    public void checkRole(Account account) {
         if (!isAdmin(account)) {
-            throw new AccessDeniedException("관리자 권한이 없습니다.");
+            try {
+                throw new AccessDeniedException("관리자 권한이 없습니다.");
+            } catch (AccessDeniedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -28,13 +35,28 @@ public class AdminService {
     }
 
     public Reservation addNewReservation(ReservationForm form) {
-        Reservation reservation = new Reservation();
-        reservation.setReservationTime(form.getReservationTime());
-        reservation.setReservationLocation(form.getReservationLocation());
+        Reservation reservation = modelMapper.map(form, Reservation.class);
         return reservationRepository.save(reservation);
     }
 
     public boolean isAlreadyReservation(String reservationTime) {
         return reservationRepository.findByReservationTime(reservationTime) != null;
+    }
+
+    public List<Reservation> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+    public void removeReservationById(Long id) {
+        reservationRepository.deleteById(id);
+    }
+
+    public Reservation findReservationById(Long id) {
+        return reservationRepository.findById(id).orElseThrow();
+    }
+
+    public void editReservation(Long id, ReservationForm form) {
+        Reservation reservation = reservationRepository.findById(id).orElseThrow();
+        modelMapper.map(form, reservation);
     }
 }
