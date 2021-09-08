34086925e47070a868613594b524ce9179281b71
SDB016 <dongbin4013@naver.com>
2021-09-08 19:22:34 +0900

    feat: View Reservations

--- src/main/java/com/alliz/account/admin/AdminController.java
+++ src/main/java/com/alliz/account/admin/AdminController.java
@@ -2,22 +2,28 @@ package com.alliz.account.admin;
 
 import com.alliz.account.CurrentAccount;
 import com.alliz.domain.Account;
+import com.alliz.reservation.Reservation;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.mail.Message;
 import javax.validation.Valid;
 import java.nio.file.AccessDeniedException;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
 
     private final AdminService adminService;
+    private final ModelMapper modelMapper;
 
     @GetMapping("/admin")
     public String adminHomeView(@CurrentAccount Account account, Model model) {
@@ -25,16 +31,16 @@ public class AdminController {
         return "admin/index";
     }
 
-    @GetMapping("/admin/create-reservations")
-    public String createReservationsView(@CurrentAccount Account account, Model model) throws AccessDeniedException {
+    @GetMapping("/admin/new-reservations")
+    public String createReservationsView(@CurrentAccount Account account, Model model) {
         adminService.checkRole(account);
         model.addAttribute(account);
         model.addAttribute(new ReservationForm());
         return "admin/new-reservations";
     }
 
-    @PostMapping("/admin/reservations/add")
-    public String addReservation(@CurrentAccount Account account, @Valid ReservationForm form, Errors errors, Model model, RedirectAttributes attributes) throws AccessDeniedException {
+    @PostMapping("/admin/reservation/add")
+    public String addReservation(@CurrentAccount Account account, @Valid ReservationForm form, Errors errors, Model model, RedirectAttributes attributes) {
         adminService.checkRole(account);
         if (errors.hasErrors()) {
             model.addAttribute(account);
@@ -50,6 +56,15 @@ public class AdminController {
 
         String message = "[장소: " + form.getReservationLocation() + ", 시간: " + form.getReservationTime() + "] 차량 시간을 등록했습니다.";
         attributes.addFlashAttribute("message", message);
-        return "redirect:/admin/create-reservations";
+        return "redirect:/admin/new-reservations";
+    }
+
+    @GetMapping("/admin/reservations")
+    public String reservationsView(@CurrentAccount Account account, Model model) {
+        adminService.checkRole(account);
+        List<Reservation> reservationList = adminService.getAllReservations();
+        model.addAttribute(reservationList);
+        model.addAttribute(account);
+        return "admin/reservations";
     }
 }
